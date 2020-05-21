71f34f962831382444b8a199e43ce05f0bcca135
klaudz <klaudzliang@gmail.com>
2020-05-21 22:43:46 +0800

    feature(Provision): Add `Provision` which parse info, entitlements, certificates of provision profiles

---
+++ xcodeflow/provision.rb
@@ -0,0 +1,53 @@
+
+require_relative 'certificate'
+require 'CFPropertyList'
+
+module Xcodeflow
+
+    class Provision
+
+        attr_reader :plist, :hash
+        
+        def initialize(path)
+            @plist = `security cms -D -i "#{path}"`
+            status = $?
+            unless status.success?
+                raise "security: unable to open \"#{path}\" for reading"
+            end
+            @hash = CFPropertyList.native_types(CFPropertyList::List.new(:data => @plist).value)
+            _load_info
+            _load_entitlements
+            _load_certificates
+        end
+        def self.open(path)
+            self.new(path)
+        end
+
+        attr_reader :app_id_name, :app_id, :team_name, :platforms, :uuid, :create_date, :expiration_date
+        attr_reader :is_xcode_managed
+        attr_reader :entitlements
+        attr_reader :certificates
+        
+        def _load_info
+            @app_id_name = @hash["AppIDName"]
+            @app_id = @hash["Entitlements"]["application-identifier"] if @hash["Entitlements"]
+            @team_name = @hash["TeamName"]
+            @platforms = @hash["Platform"]
+            @uuid = @hash["UUID"]
+            @create_date = @hash["CreationDate"]
+            @expiration_date = @hash["ExpirationDate"]
+            @is_xcode_managed = @hash["IsXcodeManaged"]
+        end
+        private :_load_info
+        
+        def _load_entitlements
+            @entitlements = @hash["Entitlements"]
+        end
+        private :_load_entitlements
+
+        def _load_certificates
+        end
+        private :_load_certificates
+
+    end
+end
