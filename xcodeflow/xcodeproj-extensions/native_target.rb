be17efcb6f4704767848c2a71f5b57f6f6182c71
klaudz <klaudzliang@gmail.com>
2020-05-29 01:31:04 +0800

    feature(Project): Cache `General` and `Signing` in `AbstractTarget` instances as reader attributes

--- xcodeflow/xcodeproj-extensions/native_target.rb
+++ xcodeflow/xcodeproj-extensions/native_target.rb
@@ -8,13 +8,21 @@ module Xcodeproj
         module Object
 
             class AbstractTarget
-                
+
+                attr_reader :xcodeflow_general
                 def xcodeflow_general(build_configuration_name)
-                    Xcodeflow::Project::General.new(self, build_configuration_name)
+                    unless @xcodeflow_general
+                        @xcodeflow_general = Xcodeflow::Project::General.new(self, build_configuration_name)
+                    end
+                    @xcodeflow_general
                 end
 
+                attr_reader :xcodeflow_signing
                 def xcodeflow_signing(build_configuration_name)
-                    Xcodeflow::Project::Signing.new(self, build_configuration_name)
+                    unless @xcodeflow_signing
+                        @xcodeflow_signing = Xcodeflow::Project::Signing.new(self, build_configuration_name)
+                    end
+                    @xcodeflow_signing
                 end
 
             end
