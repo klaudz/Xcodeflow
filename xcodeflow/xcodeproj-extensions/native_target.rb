a9c9f61433cf16fd7d664c6b844715b0b578645d
klaudz <klaudzliang@gmail.com>
2020-05-29 01:34:14 +0800

    refactor(Project): Rename the prefix `xcodeflow_` to `xcf_`

--- xcodeflow/xcodeproj-extensions/native_target.rb
+++ xcodeflow/xcodeproj-extensions/native_target.rb
@@ -9,20 +9,20 @@ module Xcodeproj
 
             class AbstractTarget
 
-                attr_reader :xcodeflow_general
-                def xcodeflow_general(build_configuration_name)
-                    unless @xcodeflow_general
-                        @xcodeflow_general = Xcodeflow::Project::General.new(self, build_configuration_name)
+                attr_reader :xcf_general
+                def xcf_general(build_configuration_name)
+                    unless @xcf_general
+                        @xcf_general = Xcodeflow::Project::General.new(self, build_configuration_name)
                     end
-                    @xcodeflow_general
+                    @xcf_general
                 end
 
-                attr_reader :xcodeflow_signing
-                def xcodeflow_signing(build_configuration_name)
-                    unless @xcodeflow_signing
-                        @xcodeflow_signing = Xcodeflow::Project::Signing.new(self, build_configuration_name)
+                attr_reader :xcf_signing
+                def xcf_signing(build_configuration_name)
+                    unless @xcf_signing
+                        @xcf_signing = Xcodeflow::Project::Signing.new(self, build_configuration_name)
                     end
-                    @xcodeflow_signing
+                    @xcf_signing
                 end
 
             end
