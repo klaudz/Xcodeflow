9f5bca9c77e45772973cc40293240137d73cb5b3
klaudz <klaudzliang@gmail.com>
2020-05-29 19:09:29 +0800

    feature(Project/General): Add methods of modifying and saving a project general

--- xcodeflow/project/general.rb
+++ xcodeflow/project/general.rb
@@ -13,13 +13,38 @@ module Xcodeflow
                 @build_configuration = target.build_configurations.select { |build_configuration|
                     build_configuration.name == build_configuration_name
                 }.first
+                @infoplist_updated = false
                 _load_info
             end
 
+            @infoplist_updated
             attr_reader :infoplist_name, :infoplist_path, :infoplist
             attr_reader :expand_build_settings_in_infoplist
-            attr_reader :display_name, :bundle_identifier, :version, :build
-            attr_reader :deployment_target
+
+            attr_accessor :display_name, :bundle_identifier, :version, :build
+            def display_name=(value)
+                _update_infoplist_setting("CFBundleDisplayName", "PRODUCT_NAME", value)
+                @display_name = value
+            end
+            def bundle_identifier=(value)
+                _update_infoplist_setting("CFBundleIdentifier", "PRODUCT_BUNDLE_IDENTIFIER", value)
+                @bundle_identifier = value
+            end
+            def version=(value)
+                _update_infoplist_setting("CFBundleShortVersionString", "MARKETING_VERSION", value)
+                @version = value
+            end
+            def build=(value)
+                _update_infoplist_setting("CFBundleVersion", "CURRENT_PROJECT_VERSION", value)
+                @build = value
+            end
+
+            attr_accessor :deployment_target
+            def deployment_target=(value)
+                @build_configuration.build_settings["IPHONEOS_DEPLOYMENT_TARGET"] = value
+                @deployment_target = value
+            end
+
             attr_reader :app_icons_source, :app_icons_paths
 
             def _load_info
@@ -52,10 +77,10 @@ module Xcodeflow
                     return
                 end
                 @infoplist = Xcodeproj::Plist.read_from_path(path)
-                @display_name = _resolve_plist_setting("CFBundleDisplayName")
-                @bundle_identifier = _resolve_plist_setting("CFBundleIdentifier")
-                @version = _resolve_plist_setting("CFBundleShortVersionString")
-                @build = _resolve_plist_setting("CFBundleVersion")
+                @display_name = _resolve_infoplist_setting("CFBundleDisplayName")
+                @bundle_identifier = _resolve_infoplist_setting("CFBundleIdentifier")
+                @version = _resolve_infoplist_setting("CFBundleShortVersionString")
+                @build = _resolve_infoplist_setting("CFBundleVersion")
             end
             private :_load_info_from_infoplist
 
@@ -80,7 +105,7 @@ module Xcodeflow
             end
             private :_load_app_icons
 
-            def _resolve_plist_setting(key)
+            def _resolve_infoplist_setting(key)
                 value = @infoplist[key]
                 return value unless @expand_build_settings_in_infoplist
                 return value unless value
@@ -91,7 +116,31 @@ module Xcodeflow
                 }
                 return value
             end
-            private :_resolve_plist_setting
+            private :_resolve_infoplist_setting
+
+            def _update_infoplist_setting(infoplist_key, build_setting_key, value)
+                infoplist_updated = true
+                @build_configuration.build_settings[build_setting_key] = value
+                if @expand_build_settings_in_infoplist
+                    @infoplist[infoplist_key] = "$(#{build_setting_key})"
+                else
+                    @infoplist[infoplist_key] = value
+                end
+            end
+            private :_update_infoplist_setting
+
+            def save
+                @target.project.save
+                _save_infoplist
+            end
+
+            def _save_infoplist
+                if @infoplist_updated
+                    Xcodeproj::Plist.write_to_path(@infoplist, @infoplist_path)
+                    @infoplist_updated = false
+                end
+            end
+            protected :_save_infoplist
 
         end
     end
