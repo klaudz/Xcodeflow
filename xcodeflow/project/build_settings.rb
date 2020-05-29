f8b7136e8af7420dc6ae8d0a5dab3c44dd8662b6
klaudz <klaudzliang@gmail.com>
2020-05-30 00:59:45 +0800

    feature(Project/BuildSettings): Add method `array_setting_add_items` for modifying array-type build settings

--- xcodeflow/project/build_settings.rb
+++ xcodeflow/project/build_settings.rb
@@ -34,6 +34,39 @@ module Xcodeflow
                 @build_configuration.resolve_build_setting(key)
             end
 
+            def _array_setting(key)
+                setting = self[key]
+                if setting.is_a?(String)
+                    # If `setting` is `String` type, convert it to `Array` type
+                    setting = setting.empty? ? [] : [ setting ]
+                end
+                setting
+            end
+            private :_array_setting
+
+            def _array_setting_by_split_string_setting(string_setting)
+                # Note:
+                #   The implementation is copied from
+                #   `Xcodeproj::Project::Object::XCBuildConfiguration.split_build_setting_array_to_string`
+                regexp = / *((['"]?).*?[^\\]\2)(?=( |\z))/
+                string.scan(regexp).map(&:first)
+            end
+            private :_array_setting_by_split_string_setting
+
+            def array_setting_add_items(key, items)
+                array_setting = _array_setting(key)
+                if array_setting.nil?
+                    array_setting = [ '$(inherited)' ]
+                end
+                case items
+                when String
+                    array_setting.push(items)
+                when Array
+                    array_setting.concat(items)
+                end
+                self[key] = array_setting
+            end
+
         end
     end
 end
