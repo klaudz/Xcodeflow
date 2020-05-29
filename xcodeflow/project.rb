0cf26ac5682e01e3b07a3a1fccec7b10e25d08fa
klaudz <klaudzliang@gmail.com>
2020-05-29 15:49:57 +0800

    feature(Project): Add methods to get schemes

--- xcodeflow/project.rb
+++ xcodeflow/project.rb
@@ -7,6 +7,22 @@ require_relative 'xcodeproj-extensions/native_target.rb'
 module Xcodeflow
 
     class Project < Xcodeproj::Project
+        
+        def xcf_scheme_names
+            scheme_names = Xcodeflow::Project.schemes(@path)
+            scheme_names
+        end
+
+        def xcf_schemes
+            scheme_names = Xcodeflow::Project.schemes(@path)
+            schemes = scheme_names.map { |name|
+                scheme_path = File.join(Xcodeproj::XCScheme.shared_data_dir(@path), name + ".xcscheme")
+                scheme = Xcodeproj::XCScheme.new(scheme_path)
+                scheme
+            }
+            schemes
+        end
+
     end
 
 end
