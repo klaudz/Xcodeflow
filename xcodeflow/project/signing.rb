cc926fc8e59621e203627d6e58512476ab312074
klaudz <klaudzliang@gmail.com>
2020-06-06 20:40:48 +0800

    refactor(Project/Signing): Remove preloading variables, and optimize the getters of variables

--- xcodeflow/project/signing.rb
+++ xcodeflow/project/signing.rb
@@ -14,44 +14,52 @@ module Xcodeflow
                 @build_configuration = target.build_configurations.select { |build_configuration|
                     build_configuration.name == build_configuration_name
                 }.first
-                _load_info
             end
 
-            attr_accessor :team_id, :bundle_identifier, :provision_profile_specifier, :signing_certificate_identity, :auto_manage_signing
+            attr_accessor :team_id
+            def team_id
+                @build_configuration.resolve_build_setting("DEVELOPMENT_TEAM")
+            end
             def team_id=(value)
                 @build_configuration.build_settings["DEVELOPMENT_TEAM"] = value
-                @team_id = value
+            end
+
+            attr_accessor :bundle_identifier
+            def bundle_identifier
+                @build_configuration.resolve_build_setting("PRODUCT_BUNDLE_IDENTIFIER")
             end
             def bundle_identifier=(value)
                 @build_configuration.build_settings["PRODUCT_BUNDLE_IDENTIFIER"] = value
-                @bundle_identifier = value
+            end
+
+            attr_accessor :provision_profile_specifier
+            def provision_profile_specifier
+                @build_configuration.resolve_build_setting("PROVISIONING_PROFILE_SPECIFIER")
             end
             def provision_profile_specifier=(value)
                 @build_configuration.build_settings["PROVISIONING_PROFILE_SPECIFIER"] = value
-                @provision_profile_specifier = value
+            end
+
+            attr_accessor :signing_certificate_identity
+            def signing_certificate_identity
+                @build_configuration.resolve_build_setting("CODE_SIGN_IDENTITY")
             end
             def signing_certificate_identity=(value)
                 @build_configuration.build_settings["CODE_SIGN_IDENTITY"] = value
-                @signing_certificate_identity = value
+            end
+
+            attr_accessor :auto_manage_signing
+            def auto_manage_signing
+                auto_manage_signing_string = @build_configuration.resolve_build_setting("CODE_SIGN_STYLE")
+                if auto_manage_signing_string.nil? or auto_manage_signing_string == "Automatic"
+                    return true
+                else
+                    return false
+                end
             end
             def auto_manage_signing=(value)
                 @build_configuration.build_settings["CODE_SIGN_STYLE"] = value ? "Automatic" : "Manual"
-                @auto_manage_signing = value
             end
-
-            def _load_info
-                @team_id = @build_configuration.resolve_build_setting("DEVELOPMENT_TEAM")
-                @bundle_identifier = @build_configuration.resolve_build_setting("PRODUCT_BUNDLE_IDENTIFIER")
-                @provision_profile_specifier = @build_configuration.resolve_build_setting("PROVISIONING_PROFILE_SPECIFIER")
-                @signing_certificate_identity = @build_configuration.resolve_build_setting("CODE_SIGN_IDENTITY")
-                auto_manage_signing_string = @build_configuration.resolve_build_setting("CODE_SIGN_STYLE")
-                if auto_manage_signing_string.nil? or auto_manage_signing_string == "Automatic"
-                    @auto_manage_signing = true
-                else
-                    @auto_manage_signing = false
-                end
-            end
-            private :_load_info
 
             def update_from_provision(provision)
                 self.team_id = provision.team_id
